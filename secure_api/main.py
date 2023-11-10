6b9381689fd6c09e7d965822af6a0ce13f2c9b38
isaacalao <88806554+isaacalao@users.noreply.github.com>
2023-11-10 14:51:51 -0500

    Changes to be committed: 	modified:   secure_api/main.py 	new file:   static/save.html 	modified:   static/style.css

--- secure_api/main.py
+++ secure_api/main.py
@@ -1,6 +1,6 @@
-from fastapi import ( FastAPI, Depends, File, Response, HTTPException,  UploadFile, status )
-from fastapi.security import ( APIKeyCookie, HTTPBearer, OAuth2PasswordBearer )
-from fastapi.responses import HTMLResponse
+from fastapi import ( FastAPI, Depends, File, Form, Response, HTTPException,  UploadFile, status )
+from fastapi.security import ( APIKeyCookie, HTTPBearer, HTTPBasic, HTTPBasicCredentials, OAuth2PasswordBearer )
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from . import schema
 
@@ -10,9 +10,8 @@ app = FastAPI(
     #docs_url="docs-something",
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
+security = HTTPBasic()
 token_auth_scheme = HTTPBearer()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 # Keep in mind dynamic routes/endpoints should be below static ones
@@ -24,14 +23,17 @@ async def root():
         content = 
             """
                 <head>
-                <title/>API Frontend</title>
+                <title>API Frontend</title>
                 <link rel="icon"  href="/static/cloud-computing.png">
                 <link rel="stylesheet" href="/static/style.css">
                 <script type="text/javascript" src="/static/func.js"></script>
                 </head>
-                
+                <h1>Login</h1>
                 <body>
+
                 <form action="/files/" enctype="multipart/form-data" method="post">
+                    <b>Password:</b> <br><input class="mybutton" name="password" id="ignore-hover" placeholder="Enter password here" type="password">
+                    <br><br>
                     <input name="files" id="real-button" type="file" onchange="console.log(this.files)" multiple hidden>
                     <label class="mybutton" for="real-button">Choose Files</label>
                     <span id="file-chosen">No file chosen.</span>
@@ -51,24 +53,30 @@ async def pub_endpoint_handler():
             "message": "Public endpoint."
     }
 
-@app.get("/priv-endpoint")
-async def priv_endpoint_handler(token: str = Depends(token_auth_scheme)):
+@app.post("/files")
+#async def priv_endpoint_handler(token: str = Depends(token_auth_scheme)):
+async def priv_endpoint_handler(files: list[UploadFile] = File(description="Multiple files may be uploaded!"), password: str = Form()):
     """An access/authentication token is required to access this endpoint."""
-    if token.credentials == "admin":
-        return {
-                "status": status.HTTP_200_OK,
-                "message": "Private endpoint",
-                "token": token
-        }
-    else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access token!")
-
-@app.post("/files/")
-async def create_files(files: list[UploadFile] = File(description="Multiple files may be uploaded!")):
     file_name_and_size = [(file.filename, file.size) for file in files]
+    print(password)
+
+    if password == "admin":
+        Redire
+        return JSONResponse(ontent=
+            {
+                "status": status.HTTP_200_OK,
+                "message": "Access granted to private endpoint",
+                "file_sizes": [ file_name_and_size ],
+                "token": password
+            }, headers={"Authorization": f'Bearer {password}'}
+        )
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password!")
+
+@app.get("/do-files")
+async def handle(creds: str = Depends(token_auth_scheme)):
     return {
-        "status": status.HTTP_200_OK,
-        "file_sizes": [ file_name_and_size ]
+        "success", "tes"
     }
 
 """
